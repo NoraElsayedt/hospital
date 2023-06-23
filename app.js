3e48a23b0a76a7ddde800c7106b0dac23e0fa993
NoraElsayedt <noraelsayed01224@gmail.com>
2023-06-23 23:20:20 +0300

    first commit

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
 const express = require('express')
 const  dotenv = require('dotenv');
+const { v4: uuidv4 } = require("uuid");
 const morgan=require('morgan');
 dotenv.config({path:"config.env"})
 const dbconection=require("./config/database");
 const path = require('path')
-
 dbconection();
 const app = express()
+const server = require("http").Server(app);
+app.set("view engine", "ejs");
+const io = require("socket.io")(server, {
+  cors: {
+    origin: '*'
+  }
+});
+const { ExpressPeerServer } = require("peer");
+const opinions = {
+  debug: true,
+}
+
+app.use("/peerjs", ExpressPeerServer(server, opinions));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.redirect(`/${'chatroom'}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId, userName) => {
+    socket.join(roomId);
+    setTimeout(()=>{
+      socket.to(roomId).broadcast.emit("user-connected", userId);
+    }, 1000)
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message, userName);
+    });
+  });
+});
+
 
 if(process.env.NODE_ENV =="development"){
     app.use(morgan('dev'));
@@ -63,6 +97,6 @@ app.use(compression())
 
 const port=process.env.PORT
 
-app.listen(port,()=>{
+server.listen(port,()=>{
 console.log(`app working of port ${port}`)
 })
